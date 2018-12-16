7613886cf06e907296bede0f6d993fdca8ba6d8d
Daniel Brown <fireflip@gmail.com>
2018-12-16 15:04:42 -0600

    Remove post formats

--- library/starter.php
+++ library/starter.php
@@ -165,26 +165,6 @@ function starter_theme_support() {
 	    )
 	);
 
-	// rss thingy
-	add_theme_support('automatic-feed-links');
-
-	// to add header image support go here: http://themble.com/support/adding-header-background-image-support/
-
-	// adding post format support
-	add_theme_support( 'post-formats',
-		array(
-			'aside',             // title less blurb
-			'gallery',           // gallery of images
-			'link',              // quick link to other site
-			'image',             // an image
-			'quote',             // a quick quote
-			'status',            // a Facebook like status update
-			'video',             // video
-			'audio',             // audio
-			'chat'               // chat transcript
-		)
-	);
-
 	// wp menus
 	add_theme_support( 'menus' );
 
--- single.php
+++ single.php
@@ -9,14 +9,42 @@
 
 						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<?php
-								/*
-								 *
-								 * If you want to remove post formats, just delete the post-formats folder and
-								 * replace the function below with the contents of the "format.php" file.
-								*/
-								get_template_part( 'post-formats/format', get_post_format() );
-							?>
+							<article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article" itemscope itemprop="blogPost" itemtype="http://schema.org/BlogPosting">
+
+                <header class="article-header entry-header">
+
+                  <h1 class="entry-title single-title" itemprop="headline" rel="bookmark"><?php the_title(); ?></h1>
+
+                  <p class="byline entry-meta vcard">
+
+                    <?php printf( __( 'Posted', 'startertheme' ).' %1$s %2$s',
+                       /* the time the post was published */
+                       '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
+                       /* the author of the post */
+                       '<span class="by">'.__( 'by', 'startertheme' ).'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
+                    ); ?>
+
+                  </p>
+
+                </header> <?php // end article header ?>
+
+                <section class="entry-content cf" itemprop="articleBody">
+
+                  <?php the_content(); ?>
+									
+                </section> <?php // end article section ?>
+
+                <footer class="article-footer">
+
+                  <?php printf( __( 'filed under', 'startertheme' ).': %1$s', get_the_category_list(', ') ); ?>
+
+                  <?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'startertheme' ) . '</span> ', ', ', '</p>' ); ?>
+
+                </footer> <?php // end article footer ?>
+
+                <?php //comments_template(); ?>
+
+              </article> <?php // end article ?>
 
 						<?php endwhile; ?>
 
