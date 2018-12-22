57a74d33bb58fef7f3aff52f7d0a4d34ae53c798
Daniel Brown <fireflip@gmail.com>
2018-12-22 13:26:37 -0600

    Establish slide structure

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -127,11 +127,5 @@ jQuery(document).ready(function($) {
         $(".nav").toggleClass("show");
 				$("#mobile-nav").toggleClass("show");
 			});
-  /*
-   * Let's fire off the gravatar function
-   * You can remove this if you don't need it
-  */
-  loadGravatars();
-
 
 }); /* end of as page load scripts */
