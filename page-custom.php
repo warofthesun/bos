19f498daabc9b16387a8c04d50d5a85e0be1e0d9
Daniel Brown <fireflip@gmail.com>
2018-12-22 10:49:03 -0600

    Use CSS based slider

--- page-custom.php
+++ page-custom.php
@@ -10,59 +10,64 @@
 
 				<div id="inner-content" class="wrap  row">
 
-						<main id="main" class="col-xs-12 col-sm-8 col-lg-9 " role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+						<main id="main" class="col-xs-12" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+							<a id="nav0"></a>
+						  <a id="nav1"></a>
+						  <a id="nav2"></a>
+						  <a id="nav3"></a>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( '' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+						  <section id="slide_container">
+						      <div class="parallax-layer" id="slide01">
+						          <p>Welcome</p>
+						          <div class="navlinks">
+						            <ul id="mynav">
+						              <li><a href="#nav0">Slide01</a></li>
+						              <li><a href="#nav1">Slide02</a></li>
+						              <li><a href="#nav2">Slide03</a></li>
+						              <li><a href="#nav3">Slide04</a></li>
+						            </ul>
+						          </div>
+						      </div>
 
-								<header class="article-header">
+						      <div class="parallax-layer" id="slide02">
+						          <p>Team</p>
+						          <div class="navlinks">
+						            <ul id="mynav">
+						              <li><a href="#nav0">Slide01</a></li>
+						              <li><a href="#nav1">Slide02</a></li>
+						              <li><a href="#nav2">Slide03</a></li>
+						              <li><a href="#nav3">Slide04</a></li>
+						            </ul>
+						          </div>
+						      </div>
 
-									<h1 class="page-title"><?php the_title(); ?></h1>
+						      <div class="parallax-layer" id="slide03">
+						          <p>Projects</p>
+						          <div class="navlinks">
+						            <ul id="mynav">
+						              <li><a href="#nav0">Slide01</a></li>
+						              <li><a href="#nav1">Slide02</a></li>
+						              <li><a href="#nav2">Slide03</a></li>
+						              <li><a href="#nav3">Slide04</a></li>
+						            </ul>
+						          </div>
+						      </div>
 
-									<p class="byline vcard">
-										<?php printf( __( 'Posted <time class="updated" datetime="%1$s" itemprop="datePublished">%2$s</time> by <span class="author">%3$s</span>', 'startertheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
-									</p>
-
-
-								</header>
-
-								<section class="entry-content " itemprop="articleBody">
-
-									<?php	the_content(); ?>
-
-								</section>
-
-
-								<footer class="article-footer">
-
-                  <?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'startertheme' ) . '</span> ', ', ', '</p>' ); ?>
-
-								</footer>
-
-								<?php comments_template(); ?>
-
-							</article>
-
-							<?php endwhile; else : ?>
-
-									<article id="post-not-found" class="hentry ">
-											<header class="article-header">
-												<h1><?php _e( 'Oops, Post Not Found!', 'startertheme' ); ?></h1>
-										</header>
-											<section class="entry-content">
-												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'startertheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'Error message', 'startertheme' ); ?></p>
-										</footer>
-									</article>
-
-							<?php endif; ?>
+						      <div class="parallax-layer" id="slide04">
+						          <p>Contact</p>
+						          <div class="navlinks">
+						            <ul id="mynav">
+						              <li><a href="#nav0">Slide01</a></li>
+						              <li><a href="#nav1">Slide02</a></li>
+						              <li><a href="#nav2">Slide03</a></li>
+						              <li><a href="#nav3">Slide04</a></li>
+						            </ul>
+						          </div>
+						      </div>
+						  </section>
 
 						</main>
-
-						<?php get_sidebar(); ?>
 
 				</div>
 
