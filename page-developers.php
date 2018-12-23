f6c54ad1c2cd080f6ff594045b95c4cefa627b49
Daniel Brown <fireflip@gmail.com>
2018-12-23 00:08:37 -0600

    Add Product Development section

--- page-developers.php
+++ page-developers.php
@@ -79,9 +79,59 @@
 							<h2><?php the_field('architecture_headline'); ?></h2>
 							<img src="<?php the_field('architecture_image'); ?>" width="100%" height="auto"/>
 						</article>
-						<article class="product_development">
-							
+						<article class="product_development wrap">
+							<h2><?php the_field('product_development_headline'); ?></h2>
+							<div class="row">
+							<?php if( have_rows('product_development_uses') ): while( have_rows('product_development_uses') ): the_row(); ?>
+							<div class="col-xs-12 col-md-4"><?php the_sub_field('column'); ?></div>
+							<?php endwhile; endif; ?>
+							</div>
+							<ul class="partner-logos">
+								<?php if( have_rows('company_logos') ): while( have_rows('company_logos') ): the_row(); ?>
+								<li class="line-item">
+									<img src="<?php the_sub_field('logo'); ?>" />
+								</li>
+							<?php endwhile; endif; ?>
+							</ul>
 						</article>
+						<article class="dark">
+							<div class="flex--column">
+								<h2><?php the_field('cloud_headline'); ?></h2>
+								<img class="cloud" src="<?php the_field('cloud_image'); ?>" width="100%" height="auto"/>
+							</div>
+						</article>
+						<div>
+							<article class="slide col-sm-12 row contact" id="contact">
+							<?php
+								$slide_image = get_field('image');
+								$size = "full"; // (thumbnail, medium, large, full or custom size)
+								$image = wp_get_attachment_image_src( $slide_image, $size );
+								// url = $image[0];
+								// width = $image[1];
+								// height = $image[2];
+							?>
+							<div class="col-xs-12 collapse top-row">
+								<div class="arrow-bottom-left"></div>
+								<div class="wrap elements row">
+									<div class="col-xs-6 left-lines"></div>
+									<div class="col-xs-6 right-lines"></div>
+								</div>
+								<div class="arrow-bottom-right"></div>
+							</div>
+							<div class="col-xs-12 collapse bottom-row" style="background-image:url('<?php echo $image[0]; ?>');">
+								<div class="overlay"></div>
+							</div>
+							<div class="col-xs-12 contact-form">
+								<div class="col-xs-10 col-md-4">
+									<h2><?php the_field('headline'); ?></h2>
+									<div class="form">
+										<h3><?php the_field('form_headline'); ?></h3>
+										<?php the_field('form'); ?>
+									</div>
+								</div>
+							</div>
+						</article>
+						</div>
 				</div>
 
 			</div>
