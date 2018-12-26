36850d6617ce4969991a36df57bc41d3b5b14494
Daniel Brown <fireflip@gmail.com>
2018-12-25 19:24:09 -0600

    Add pagepiling script

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -136,4 +136,21 @@ jQuery(document).ready(function($) {
 		$(this).parent().addClass('selected').siblings().removeClass('selected');
 	});
 
+	$('#pagepiling').pagepiling({
+		menu: '#menu',
+		anchors: ['page1', 'page2', 'page3', 'page4'],
+		sectionsColor: ['white', '#ee005a', '#2C3E50', '#39C'],
+		navigation: false,
+		afterRender: function(){
+			$('#pp-nav').addClass('custom');
+		},
+		afterLoad: function(anchorLink, index){
+			if(index>1){
+				$('#pp-nav').removeClass('custom');
+			}else{
+				$('#pp-nav').addClass('custom');
+			}
+		}
+});
+
 }); /* end of as page load scripts */
--- page-slider.php
+++ page-slider.php
@@ -1,41 +1,53 @@
 <!--page-founders-->
 <?php get_header(); ?>
-<style>
-.footer {
-	height: 0;
-	display: none;
-}
-</style>
+
+
 			<div id="content" class="founders">
 
 				<div id="inner-content" class="wrap wrap--wider row">
 
 						<main id="main" class="col-xs-12" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog" style="background-color:red;">
-							<div class="cachu__container">
 
-									<!-- All sections are wrapped inside .cachu__sections  -->
+							<div id="pagepiling">
+								<ul id="menu" style="position:absolute;width: 100px; top:50%;z-index:50;left:30px;">
+									<li class="active"><a href="#page1">Page 1</a></li>
+									<li ><a href="#page2">Page 2</a></li>
+									<li ><a href="#page3">Page 3</a></li>
+									<li ><a href="#page4">Page 4</a></li>
+								</ul>
+							    <div class="section" id="section1">
+							    	<h1>pagePiling.js</h1>
+									<p>Create an original scrolling site</p>
+									<img src="imgs/pagePiling-plugin.gif" alt="pagePiling" />
+									<br />
 
-									<div class="cachu__sections">
-										<section class="cachu__section">Some section</section>
-										<section class="cachu__section">Some section</section>
-										<section class="cachu__section" id="contact">Contact</section>
-									</div>
-
-									<!-- End of section  -->
-									<!-- Navigation will be dynamically added here (via JavaScript) -->
-									</div>
-									<script>
-									const options = {
-										  disableMouseEvents: false, // Disable mousewheel event listening.
-										  scrollingSpeed: 2000,  // The speed of the transition.
-										  scrollingLoop: false,  // Loop after reaching the end.
-										  navigationEnabled: true, // Enable navigation buttons
-										  navigationPosition: 'left'  // The Navigation's position
-										};
-
-										let slider = new Cachu(document.querySelector('.cachu__container'), options);
-										slider.run();
-									</script>
+							    </div>
+							    <div class="section" id="section2">
+							    	<div class="intro">
+							    		<div id="colors"></div>
+							    		<h1>jQuery plugin</h1>
+							    		<p>Pile your sections one over another and access them scrolling or by URL!</p>
+							    		<div id="markup">
+							    			<script src="https://gist.github.com/alvarotrigo/4a87a4b8757d87df8a72.js"></script>
+							    		</div>
+							    	</div>
+							    </div>
+							    <div class="section" id="section3">
+							    	<div class="intro">
+							    		<h1>Configurable</h1>
+							    		<p>Plenty of options, methods and callbacks to use.</p>
+							    		<div id="colors2"></div>
+							    		<div id="colors3"></div>
+							    	</div>
+							    </div>
+							    <div class="section" id="section4">
+							    	<div class="intro">
+							    		<h1>Compatible</h1>
+							    		<p>Designed to work on tablet and mobile devices.</p>
+							    		<p>Oh! And its compatible with old browsers such as IE 8 or Opera 12!</p>
+							    	</div>
+							    </div>
+							</div>
 						</main>
 
 				</div>
