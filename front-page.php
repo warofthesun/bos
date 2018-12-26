8e37565647f767a752ac87d08dfc5803ff016806
Daniel Brown <fireflip@gmail.com>
2018-12-26 15:47:19 -0600

    Testimonial slider in place

--- front-page.php
+++ front-page.php
@@ -37,28 +37,39 @@
 										</div>
 									</div>
 								</div>
-								<div class="testimonials">
-									<div class="container flex flex--row flex--row__mobile-collapse">
-										<div class="col-xs-0 col-md-2">
+								<?php endwhile; endif; ?>
+								<div id="carousel">
+									<div id="carousel--buttons">
+										<a id="prev" href="#"></a>
+										<a id="next" href="#"></a>
+									</div>
+									<div id="slides">
+										<ul class="testimonials">
+											<?php if( have_rows('testimonials') ) : while( have_rows('testimonials') ) : the_row(); ?>
 											<?php
-												$attachment_id = get_field('headshot');
+												$attachment_id = get_sub_field('headshot');
 												$size = "full"; // (thumbnail, medium, large, full or custom size)
 												$image = wp_get_attachment_image_src( $attachment_id, $size );
 												// url = $image[0];
 												// width = $image[1];
 												// height = $image[2];
 											?>
-											<div class="avatar" style="background-image:url('<?php echo $image[0]; ?>');"></div>
-										</div>
-										<div class="col-xs-10 testimonial">
-											<?php the_field('testimonial'); ?>
-											<div class="user">
-												<span class="name"><?php the_field('name'); ?> </span><span class="title"><?php the_field('position'); ?></span>
-											</div>
-										</div>
+											<li class="container flex flex--row flex--row__mobile-collapse slide">
+												<div class="col-xs-0 col-md-2">
+
+													<div class="avatar" style="background-image:url('<?php echo $image[0]; ?>');"></div>
+												</div>
+												<div class="col-xs-10 testimonial">
+													<?php the_sub_field('testimonial'); ?>
+													<div class="user">
+														<span class="name"><?php the_sub_field('name'); ?> </span><span class="title"><?php the_sub_field('position'); ?></span>
+													</div>
+												</div>
+											</li>
+											<?php endwhile; endif; ?>
+										</ul>
 									</div>
 								</div>
-							<?php endwhile; endif; ?>
 
 						</main>
 
